3a71cec9d504f4781e95e88b3b49f96e50ec5691
Idemar Neto <idemarneto13@gmail.com>
2025-06-04 17:09:12 -0300

    #6 | PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Calculadora de Progressão',
+        short_name: 'One Up',
+        description: 'App para acompanhar progressão de força na academia',
+        theme_color: '#111827',
+        background_color: '#111827',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
